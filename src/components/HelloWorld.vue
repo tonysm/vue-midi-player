67785515cc69ba2c09741c16f430787e28f47bdc
Tony Messias <tonyzrp@gmail.com>
2018-08-05 23:16:03 -0300

    Vue MIDI player

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,97 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div class="hello">
+        <h1>MIDI Player</h1>
+
+        <button @click="stop" v-if="playing">Stop</button>
+
+        <form @submit.prevent="play">
+            <div class="form-group">
+                <label >Song</label>
+                <select v-model="song">
+                    <option :value="option" v-for="option in songs" :key="option.name">{{ option.name }}</option>
+                </select>
+            </div>
+
+            <div class="form-group">
+                <label for="">Instrument</label>
+                <select v-model="instrument">
+                    <option :value="option" v-for="option in instruments" :key="option.name">{{ option.name }}</option>
+                </select>
+            </div>
+
+            <button :disabled="playing || !song || !instrument">
+                Play
+            </button>
+        </form>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+    import { Player } from 'midi-player-js';
+    import Soundfont from 'soundfont-player';
+    import axios from 'axios';
+
+    export default {
+        name: 'HelloWorld',
+        data () {
+            return {
+                song: null,
+                instrument: null,
+                playing: false,
+                songs: [
+                    { name: 'Everlong - Foo Fighters', filename: 'Foo_Fighters_-_Everlong.mid' },
+                    { name: "Jurassic Park Movie Theme", filename: "Movie_Themes_-_Jurassic_Park.mid" },
+                    { name: "Claire De Lune - Claude Debussy", filename: "clairdelune.mid" },
+                ],
+                instruments: [
+                    { name: 'Piano', filename: 'acoustic_grand_piano-mp3' },
+                    { name: 'Accordion', filename: 'accordion-mp3' },
+                    { name: 'Acoustic Guitar Nylon', filename: 'acoustic_guitar_nylon-mp3' },
+                    { name: 'Acoustic Guitar Steel', filename: 'acoustic_guitar_steel-mp3' },
+                ],
+            };
+        },
+        methods: {
+            stop() {
+                this.playing = false;
+            },
+            play() {
+                this.playing = true;
+
+                const AudioContext = window.AudioContext || window.webkitAudioContext || false;
+                const ac = new AudioContext || new webkitAudioContext;
+
+                Soundfont.instrument(ac, `https://raw.githubusercontent.com/gleitz/midi-js-soundfonts/gh-pages/MusyngKite/${this.instrument.filename}.js`).then((instrument) => {
+                    const player = new Player((event) => {
+                        if (!this.playing) {
+                            player.stop();
+                        }
+
+                        if (event.name === 'Note on' && event.velocity > 0) {
+                            instrument.play(event.noteName, ac.currentTime, {
+                                gain: event.velocity / 100,
+                            });
+                        }
+                    });
+
+                    axios.get(`/songs/${this.song.filename}`, {
+                        responseType: 'arraybuffer'
+                    }).then(({data: song}) => {
+                        player.loadArrayBuffer(song);
+
+                        // we have access to each track, and each track has events.
+                        // console.log(player.tracks);
+                        player.play();
+                    });
+                });
+            }
+        }
+    }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+    h3 {
+        margin: 40px 0 0;
+    }
 </style>
